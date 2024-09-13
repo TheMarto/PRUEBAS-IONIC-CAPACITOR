c1e208fe9f20a1d7e6ff5554fcc29022e6e4b165
martin <arnimartin@gmail.com>
2024-09-13 19:47:48 +0200

    P2 12SEP24

---
+++ second-project-shoppinglist/src/app/services/shopping-services.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingServicesService {
+
+  //variables
+  public items: string [];
+  public isEmpty: boolean;
+
+  constructor() {
+
+    this.items = [];
+    this.isEmpty = true;
+
+  }
+
+  //methods
+
+  //add item
+  addItems(item:string){
+    this.items.push(item);
+    this.isEmpty = false;
+  }
+
+  //Remuve item
+  remuveItem(item:string){
+    let index = this.items.findIndex(it => it.toUpperCase().trim() === item.toUpperCase().trim()); //toUppserCase hace mayusculas y trim elimina espacios
+    if(index !=-1){
+      this.items.splice(index, 1); //desde que posición empiezo and que eliminamos
+      if(this.items.length == 0){
+        this.isEmpty = true;
+      }
+    }
+  }
+
+  //remuve all items
+  remuveAll(){
+    this.items = [];
+    this.isEmpty = true;
+  }
+
+  //Find
+  existItem(item: string){
+    const itemFound = this.items.find(it => it.toLocaleUpperCase().trim() === item.toLocaleUpperCase().trim());
+    return itemFound;
+  }
+}
