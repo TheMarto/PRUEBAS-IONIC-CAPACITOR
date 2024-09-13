bc378ab59e4d5744195d0b7f7dbafb8e292cecaf
martin <arnimartin@gmail.com>
2024-09-13 19:48:24 +0200

    P2 13SEP24

--- first-project-ionic/capacitor.config.ts
+++ first-project-ionic/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'first-project-ionic',
+  appName: 'first-project-ionic-couter',
   webDir: 'www'
 };
 
