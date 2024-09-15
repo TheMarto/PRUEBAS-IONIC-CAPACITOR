e1fef890d6f6624bdda58a2e8f3ab2aaa0dcd395
TheMarto <arnimartin@gmail.com>
2024-09-15 18:25:20 +0200

    P4 15SEP24

---
+++ thir-project-flashlight/src/app/home/home.page.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CapacitorFlash } from '@capgo/capacitor-flash';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  public active: boolean;
+
+  constructor() {
+    this.active = false;
+  }
+
+  flash(){
+    this.active = !this.active;
+
+    if(this.active){
+      CapacitorFlash.switchOn({ intensity: 100 });
+      console.log(true)
+    }else{
+      CapacitorFlash.switchOff();
+      console.log(false)
+    }
+  }
+
+}
