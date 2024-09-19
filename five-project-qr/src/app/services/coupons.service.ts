f919b1fc771d53887619a57e5b54e0b6e6f7b376
martin <arnimartin@gmail.com>
2024-09-19 19:15:30 +0200

    P5 19SEP24

--- five-project-qr/src/app/services/coupons.service.ts
+++ five-project-qr/src/app/services/coupons.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Coupon } from '../models/coupon';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,17 @@ import { Injectable } from '@angular/core';
 export class CouponsService {
 
   constructor() { }
+
+
+  getCoupons(){
+    return fetch('./assets/data/data.json').then(async res=>{
+      const coupons: Coupon[] = await res.json() as Coupon[];
+      coupons.forEach(c => c.acttive = false);
+      return Promise.resolve(coupons);
+    }).catch(err =>{
+      console.log(err);
+      return Promise.reject([]);
+    })
+  }
+
 }
