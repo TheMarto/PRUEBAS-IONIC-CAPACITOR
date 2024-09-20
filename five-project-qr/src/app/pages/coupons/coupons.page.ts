bf7f8e4211226fb84456c7a3a3b657e9d5815f7a
TheMarto <arnimartin@gmail.com>
2024-09-21 01:00:50 +0200

    P5 21SEP24

--- five-project-qr/src/app/pages/coupons/coupons.page.ts
+++ five-project-qr/src/app/pages/coupons/coupons.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonFab, NavController, NavParams } from '@ionic/angular';
 import { Coupon } from 'src/app/models/coupon';
 import { CouponsService } from 'src/app/services/coupons.service';
 
@@ -9,20 +10,40 @@ import { CouponsService } from 'src/app/services/coupons.service';
 })
 export class CouponsPage implements OnInit {
 
+
+  //variables
   public coupons: Coupon[];
+  public couponsActive: boolean;
 
-  constructor(public CouponsService: CouponsService) { 
+
+  constructor(
+    public CouponsService: CouponsService,
+    private navParams: NavParams,
+    private navController: NavController,
+  ) { 
     this.coupons = [];
+    this.couponsActive = false;
   }
+ 
 
+//activamos coupos desde el oninit
   ngOnInit() {
     this.CouponsService.getCoupons().then((coupons: Coupon[])=> {
       this.coupons = coupons;
-        console.log(this.coupons);
+        //console.log(this.coupons);
       })
   }
-  changeActive(coupon: Coupon){
-     coupon.active = !coupon.active
-  }
 
+
+  //cambiar activo o no
+  changeActive(coupon: Coupon){
+     coupon.active = !coupon.active;
+     this.couponsActive = this.coupons.some(c => c.active);// devuelve true o false dependiendo de si esta activo alguno del array con some
+  }
+  //vamos a la otra pagina con la info
+  goToCard(){
+    this.navParams.data["coupons"] = this.coupons.filter(c => c.active);
+      this.navController.navigateForward('card-coupon');
+  }
+  
 }
