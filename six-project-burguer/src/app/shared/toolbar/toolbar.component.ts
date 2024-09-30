19d81b3519e7d377cb84d520a2b44cbf33e55f8c
TheMarto <arnimartin@gmail.com>
2024-10-01 00:34:31 +0200

    P6 30SEP24

--- six-project-burguer/src/app/shared/toolbar/toolbar.component.ts
+++ six-project-burguer/src/app/shared/toolbar/toolbar.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { EventType, Router, RoutesRecognized } from '@angular/router';
+import { IonicModule, NavController } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-toolbar',
@@ -11,9 +13,29 @@ import { TranslateModule } from '@ngx-translate/core';
   imports:[IonicModule, CommonModule, TranslateModule]
 })
 export class ToolbarComponent  implements OnInit {
+//methods
+public showBack: boolean;
 
-  constructor() { }
+  constructor(
+    private router: Router, //para lo del botton en el oninit
+    private navController: NavController,
+  ) { 
+    this.showBack = false;
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.router.events.pipe(
+      filter((event) => event.type == EventType.RoutesRecognized)
+    ).subscribe({
+      next: (event: RoutesRecognized) => {
+        //console.log(event.state.root.firstChild.data);
+        this.showBack = event.state.root.firstChild.data['showBack']
+      }
+    })
+  }
+//go back
+  goBack(){
+    this.navController.back()
+  }
 
 }
