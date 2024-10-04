a4dbc9b92e5b534aa04f875b7456841cfcaf8c73
TheMarto <arnimartin@gmail.com>
2024-10-04 18:53:29 +0200

    P6 04OCT24

--- six-project-burguer/src/app/state/auth/auth.service.ts
+++ six-project-burguer/src/app/state/auth/auth.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { CapacitorHttp, HttpResponse } from '@capacitor/core';
+import { TokenUser } from 'src/app/models/token-user';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  //   metoth for login
+  login(email:string, password: string){
+    return CapacitorHttp.post({
+      url: environment.urlApi + 'auth/login',
+      data: {
+        email,
+        password
+      },
+      params:{},
+      headers: {
+        'content-type': 'aplication/json'
+      }      
+    }).then((response: HttpResponse)=>{
+      if(response.status == 201){
+        const data = response.data as TokenUser;
+        return data;
+      }
+      return null;
+    }).catch(err => {
+      console.error(err);
+      return null;
+    })
+  }
 
   constructor() { }
 }
--- six-project-burguer/src/app/state/auth/auth.state.ts
+++ six-project-burguer/src/app/state/auth/auth.state.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { State, Action, StateContext, select, Selector } from '@ngxs/store';
 import { Login } from './auth.actions';
 import { AuthService } from './auth.service';
+import { TokenUser } from 'src/app/models/token-user';
+import { Preferences } from '@capacitor/preferences';
+import { KEY_TOKEN } from 'src/app/constants/constans';
 
 export class AuthStateModel {
 
@@ -36,6 +39,17 @@ export class AuthState {
 
 
   @Action(Login)
-  Login({ getState, setState }: StateContext<AuthStateModel>, { payload }: Login) {
+  Login({ setState }: StateContext<AuthStateModel>, { payload }: Login) {
+    return this.AuthService.login(payload.email, payload.password).then(async (token: TokenUser)=>{
+      if(token){
+        await Preferences.set({key: KEY_TOKEN, value: token.accessToken})
+        setState: ({success: true})
+      }
+      else{
+        setState: ({success: false})
+      }
+    }).catch(err => {
+      setState: ({success: false})
+    })
   }
 }
