298dba5d5b60d4d3b50a060ac6df0328eb495cb2
TheMarto <arnimartin@gmail.com>
2024-10-09 22:01:16 +0200

    P6 09OCT24

--- six-project-burguer/src/app/shared/list-products-order/list-products-order.component.ts
+++ six-project-burguer/src/app/shared/list-products-order/list-products-order.component.ts
@@ -1,24 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 import { UserOrderService } from 'src/app/services/user-order.service';
-import { CreateAccountComponent } from '../create-account/create-account.component';
+
 
 @Component({
   selector: 'app-list-products-order',
   templateUrl: './list-products-order.component.html',
   styleUrls: ['./list-products-order.component.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, TranslateModule, CreateAccountComponent,]
+  imports: [IonicModule, CommonModule, FormsModule, TranslateModule,]
 })
 export class ListProductsOrderComponent {
 
 
-  //variables
-  public userOrderService: UserOrderService;
-  constructor() { }
+
+  
+  constructor(
+    public userOrderService: UserOrderService,
+  ) { }
  
 
 }
